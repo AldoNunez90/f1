9019c2193d27962426550227628a2ddf5b6ac9dd
aldo <aldo.andres.nunez@gmail.com>
2026-07-14 23:58:40 -0300

    lighthouse

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
-
 const nextConfig: NextConfig = {
-  
+  // Configuración de orígenes de desarrollo permitidos
   allowedDevOrigins: ['192.168.0.*'],
 
   images: {
@@ -10,30 +9,42 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'flagcdn.com',
+        pathname: '/**', // Permite cualquier subruta del CDN de banderas
       },
       {
         protocol: 'https',
         hostname: 'media.formula1.com',
+        pathname: '/**', // Permite cualquier subruta de la web de F1
       },
       {
         protocol: 'https',
-        hostname: '*.ytimg.com',
+        hostname: '*.ytimg.com', // Servidores de miniatura de YouTube (ej. i.ytimg.com)
+        pathname: '/**', // SÚPER CRÍTICO: Permite todas las carpetas y rutas internas
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com', // Servidor de respaldo que a veces usa la API de YT
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: '*.motorsport.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'objetos.estaticos-marca.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: '*.hearstapps.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: '*.f1technical.net',
+        pathname: '/**',
       },
     ],
   },
