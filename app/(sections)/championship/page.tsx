e5e927c0f3815849db4e366d3b23d16b73b1eeab
aldo <aldo.andres.nunez@gmail.com>
2026-08-07 01:51:51 -0300

    svg completo

--- app/(sections)/championship/page.tsx
+++ app/(sections)/championship/page.tsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from 'react';
 import { useF1Data } from '@/lib/hooks/useF1Data';
 import { LoadingTab } from '@/app/components/common/Loading';
 import { ErrorMessage } from '@/app/components/common/Error';
-import CircuitPathTester from '@/app/components/tester/svgTester';
 
 
 
@@ -161,7 +160,6 @@ export default function ChampionshipPage() {
           </div>
         )}
       </div>
-        <CircuitPathTester />
     </div>
   );
 }
